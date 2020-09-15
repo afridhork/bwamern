074b3eda3b819c09eface7701618f10ad06d9569
Afridho1517051173 <afridhorkartawiria@gmail.com>
2020-09-15 11:25:36 +0700

    put header to landing page

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -15,20 +15,21 @@ export default function Button(props) {
 		if (props.onClick) props.onClick();
 	};
 
-	if (props.isDisabled || props.isLoading)
+	if (props.isDisabled || props.isLoading) {
 		if (props.isDisabled) className.push('disabled');
-	return (
-		<span className={className.join(' ')} style={props.style}>
-			{props.isLoading ? (
-				<>
-					<span className='spinner-border spinner-border-sm mx-5'></span>
-					<span className='sr-only'>Loading...</span>
-				</>
-			) : (
-				props.children
-			)}
-		</span>
-	);
+		return (
+			<span className={className.join(' ')} style={props.style}>
+				{props.isLoading ? (
+					<>
+						<span className='spinner-border spinner-border-sm mx-5'></span>
+						<span className='sr-only'>Loading...</span>
+					</>
+				) : (
+					props.children
+				)}
+			</span>
+		);
+	}
 	if (props.type === 'link') {
 		if (props.isExternal) {
 			return (
--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import Button from './index';
 
 test('should not allowed click button if isDisabled is present', () => {
@@ -16,8 +18,18 @@ test('should render loading/spinner', () => {
 	expect(container.querySelector('span')).toBeInTheDocument();
 });
 
+test('should render <a> tag', () => {
+	const { container } = render(<Button type='link' isExternal></Button>);
+
+	expect(container.querySelector('a')).toBeInTheDocument();
+});
+
 test('should render <Link> tag', () => {
-	const { container } = render(<Button type='link'></Button>);
+	const { container } = render(
+		<Router>
+			<Button href='' type='link'></Button>
+		</Router>
+	);
 
 	expect(container.querySelector('a')).toBeInTheDocument();
 });
