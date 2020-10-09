a3eecd9901af67f668ede2681d088fa686e15e19
Afridho1517051173 <afridhorkartawiria@gmail.com>
2020-10-09 23:41:33 +0700

    finished parts/mostpiked + categories

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Button from "elements/Button";
-import BrandIcon from "parts/iconText";
+import BrandIcon from "parts/IconText";
 
 export default function Header(props) {
 	const getNavLinkClass = (path) => {
@@ -25,7 +25,7 @@ export default function Header(props) {
 								</Button>
 							</li>
 							<li className={`nav-item ${getNavLinkClass("/stories")}`}>
-								<Button className='nav-link' type='link' href='/sories'>
+								<Button className='nav-link' type='link' href='/stories'>
 									stories
 								</Button>
 							</li>
